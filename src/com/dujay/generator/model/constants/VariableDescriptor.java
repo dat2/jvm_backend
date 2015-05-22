756631283486759c5e24a9a76b5dc9b599f13c6d
Nicholas Dujay <nickdujay@gmail.com>
2015-05-21 22:49:49 -0400

    deleted old constants but now all code is breaking

--- src/com/dujay/generator/model/constants/VariableDescriptor.java
+++
@@ -1,9 +0,0 @@
-package com.dujay.generator.model.constants;
-
-public class VariableDescriptor extends Descriptor {
-
-  public VariableDescriptor(String name, ClassInfo classDescriptor) {
-    super(name, classDescriptor.getTypeString() + ";");
-  }
-
-}
